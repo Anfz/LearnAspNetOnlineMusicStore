899c9e36d466e84064d99dcafdc42bbeacad6e27
Chris Beaver <chrisb@pinpointcomms.com.au>
2015-11-25 22:29:41 +1000

    Added a Report Layout Template - Tested With Reviews

---
+++ MvcMusicStudio/Scripts/report.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+class Report {
+
+    constructor(model, definition) {
+
+    };
+
+
+    display() {
+        $('.report').append("<p>Using Javascript To Build HTML cause ummm not playing with angular yet?</p>" +
+            "<p> " +@Html.DisplayNameFor(model => model.Album.Title) );
+    }
+
+
+}
+
+// only for one report should really look though and define them all 
+
+
+
+var report = new Report();
+report.display();
