214d0ba5fcf41c711c218b1f6f20e39cb94f42d3
Chris Beaver <chrisb@pinpointcomms.com.au>
2015-11-26 19:45:24 +1000

    Implmented NonFactors.Mvc.Grid http://mvc-grid.azurewebsites.net/ :D

--- MvcMusicStudio/App_Start/BundleConfig.cs
+++ MvcMusicStudio/App_Start/BundleConfig.cs
@@ -17,6 +17,8 @@ namespace MvcMusicStudio
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/MvcGrid").Include(
+                      "~/Scripts/MvcGrid/mvc-grid.js"));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -29,6 +31,9 @@ namespace MvcMusicStudio
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/MvcGrid").Include(
+                      "~/Content/MvcGrid/mvc-grid.css"));
         }
     }
 }
